585f2348b38f3dc025ab56edf213fae37c85d40a
Tameem Rwalah <trwalah@gmail.com>
2022-01-28 20:33:24 +0200

    Edit Home Page

--- src/app/Home/home-component/home-component.component.ts
+++ src/app/Home/home-component/home-component.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-home-component',
@@ -7,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponentComponent implements OnInit {
 
-  constructor() { }
+  constructor(config: NgbCarouselConfig) { 
+    config.interval = 2000;  
+    config.wrap = true;  
+    config.keyboard = false;  
+    config.pauseOnHover = false;  
+  }
 
   ngOnInit(): void {
   }
